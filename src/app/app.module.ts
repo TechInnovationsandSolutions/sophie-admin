af1400311d475f99e13b5fd1795cab4554aa5e39
Ebere <ebereuzodufa@gmail.com>
2020-03-03 15:01:43 +0100

    Adds route module and common module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule, 
+    RouterModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]

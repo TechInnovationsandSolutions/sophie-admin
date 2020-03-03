d874924a8f5c7ab15bfa2b5359c35adb2e230ba6
Ebere <ebereuzodufa@gmail.com>
2020-03-03 15:14:13 +0100

    Adds required coomponents via CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,15 @@ import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
 
 @NgModule({
   imports: [
@@ -12,7 +21,16 @@ import { CommonModule } from '@angular/common';
     RouterModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CategoryComponent,
+    ProductsComponent,
+    CustomersComponent,
+    TransactionsComponent,
+    ReviewsComponent,
+    LoginComponent,
+    SidebarComponent,
+    AppLayoutComponent
   ],
   providers: [],
   bootstrap: [AppComponent]

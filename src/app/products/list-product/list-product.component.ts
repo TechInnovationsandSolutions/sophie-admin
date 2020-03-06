42db54e9c0ba2e73e4b0bf98c9f7b9bcd4095ddb
Ebere <ebereuzodufa@gmail.com>
2020-03-06 16:45:42 +0100

    Populate products using params for request & page

--- src/app/products/list-product/list-product.component.ts
+++ src/app/products/list-product/list-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DashboardServService, ProductResponse, IProduct } from './../../shared/index';
 
 @Component({
   selector: 'app-list-product',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private service: DashboardServService) { }
+
+  pagesArray: Array<number> = [];
+  currentPage: number = 1;
+  products: IProduct[] = [];
 
   ngOnInit() {
+    if (!this.route.snapshot.queryParams.page) {
+      this.router.navigate([],{ queryParams: { page: 1 } });
+      return;
+    }
+
+    const pg = this.route.snapshot.queryParams.page;
+    this.currentPage = Number(pg);
+    this.service.getProducts(pg).then(res=>{
+      // console.log(pg, res)
+      var resp = <ProductResponse>res;
+      this.pagesArray = resp.pg;
+      this.products = resp.data;
+    });
   }
 
+  setPage(val){
+    if (Number(val) === this.currentPage)
+    return;
+
+    this.currentPage = val;
+    this.router.navigate([],{ queryParams: { page: val } });
+    this.setProducts(val);
+  }
+
+  setProducts(pg){
+    this.service.getProducts(pg).then(res=>{
+      // console.log(pg, res)
+      var resp = <ProductResponse>res;
+      this.pagesArray = resp.pg;
+      this.products = resp.data;
+    });
+  }
 }

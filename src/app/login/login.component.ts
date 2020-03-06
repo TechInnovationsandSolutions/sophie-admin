597ba735710f1c3f2f48e41bffb1f659f9e94af2
Ebere <ebereuzodufa@gmail.com>
2020-03-06 16:41:04 +0100

    Enables appropriate login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { DashboardServService, AuthService } from '../shared';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private serv:DashboardServService, private auth:AuthService, private route: ActivatedRoute) { }
+
+  email:string;
+  password:string;
+  msg:string = '';
+  url_route:string = '';
 
   ngOnInit() {
+    this.url_route= this.route.snapshot.queryParams.redirectUrl ? this.route.snapshot.queryParams.redirectUrl : '/dashboard';
   }
 
+  formFocus():void{
+    this.msg = '';
+  }
+
+  submitForm(){
+    var emailRegEx = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
+    if(emailRegEx.test(this.email) && this.password){
+      this.tryLogin(this.email, this.password);
+    }
+  }
+
+  tryLogin(email:string, password:string){
+    this.auth.login(email, password).subscribe(
+      r => {
+        if(r.data.token && r.data.is_admin) {
+          this.serv.setToken(r.data.token)
+          this.router.navigateByUrl(this.url_route);
+        }
+    },
+    r => {
+      this.msg = 'Email or Password is incorrect.'
+    });
+  }
 }

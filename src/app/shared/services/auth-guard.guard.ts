f27e9fb97a12f97673f57f462a80811fc545fa2e
Ebere <ebereuzodufa@gmail.com>
2020-03-06 16:39:56 +0100

    Creates route gaurd and redirect

--- src/app/shared/services/auth-guard.guard.ts
+++ src/app/shared/services/auth-guard.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { DashboardServService } from './dashboard-serv.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardGuard implements CanActivate {
+  constructor(private serv: DashboardServService, private router: Router){}
+
   canActivate(
-    next: ActivatedRouteSnapshot,
+    route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+      const redirectUrl = route['_routerState']['url'];
+
+    if (this.serv.isLogged()) {
+      return true;
+    }
+
+    this.router.navigateByUrl(
+      this.router.createUrlTree(
+        ['/login'], {
+          queryParams: {
+            redirectUrl
+          }
+        }
+      )
+    );
+
+    return false;
   }
   
 }

dabd1eda85a765edb45cf48c61b2c047519bbffe
Ebere <ebereuzodufa@gmail.com>
2020-05-05 15:09:53 +0100

    Better login UX

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DashboardServService, AuthService } from '../shared';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +9,19 @@ import { DashboardServService, AuthService } from '../shared';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @BlockUI() blockUI: NgBlockUI;
 
-  constructor(private router: Router, private serv: DashboardServService, private auth: AuthService, private route: ActivatedRoute) { }
+  constructor(
+    private router: Router,
+    private serv: DashboardServService,
+    private auth: AuthService,
+    private route: ActivatedRoute
+  ) { }
 
   email: string;
   password: string;
   msg = '';
+  // tslint:disable-next-line: variable-name
   url_route = '';
 
   ngOnInit() {
@@ -32,12 +40,15 @@ export class LoginComponent implements OnInit {
   }
 
   tryLogin(email: string, password: string) {
-    this.auth.login(email, password).subscribe(
-      r => {
-        if (r.data.token && r.data.is_admin) {
-          this.serv.setToken(r.data.token);
-          this.router.navigateByUrl(this.url_route);
-        }
+    this.blockUI.start('Please Wait...');
+    this.auth.login(email, password).then(r => {
+      this.blockUI.stop();
+      if (r.data.token && r.data.is_admin) {
+        this.serv.setToken(r.data.token);
+        this.router.navigateByUrl(this.url_route);
+      } else {
+        this.msg = 'Email or Password is incorrect.';
+      }
     },
     r => {
       this.msg = 'Email or Password is incorrect.';

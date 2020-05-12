23ebcd97c55286bcd51dae34490117682f876923
Ebere <ebereuzodufa@gmail.com>
2020-05-12 09:46:49 +0100

    Adds model for customers

--- src/app/shared/model/customer.model.ts
+++ src/app/shared/model/customer.model.ts
@@ -1,12 +1,12 @@
 export interface ICustomer {
-    id: number;
-    first_name: string;
-    last_name: string;
-    email: string;
-    email_verified_at: string;
-    phone: string;
-    address: string;
-    created_at: Date;
-    updated_at: Date;
-    deleted_at: string;
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  email_verified_at: string;
+  phone: string;
+  address: string;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: string;
 }
